6ea85e8229c12b38b0be46e2559b93ff541adb9b
Class4vet <bb.company.2020@gmail.com>
2025-07-20 20:44:20 +0900

    Fix basename routing for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 
+// GitHub Pages를 위한 basename 설정
+const basename = window.location.hostname === 'jinyoung89.github.io' ? '/hypecon' : '';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/hypecon">
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
